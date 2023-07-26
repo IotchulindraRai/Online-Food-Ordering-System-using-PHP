919c468060757d7036d7a1218d8c7645e17ecedf
Chulindra Rai <87846923+IotchulindraRai@users.noreply.github.com>
2023-07-27 01:00:26 +0530

    Update managersignup.php

--- managersignup.php
+++ managersignup.php
@@ -212,7 +212,7 @@
 
   <footer class="container-fluid bg-4 text-center">
   <br>
-  <p> Food Exploria 2017 | &copy All Rights Reserved </p>
+  <p> Developed by Chulindra | &copy; All Rights Reserved </p>
   <br>
   </footer>
 </html>
